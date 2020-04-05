49036febf94112f52437a556f5894c6139426019
jsalisi <jsalisi@gmail.com>
2020-04-04 17:49:45 -0700

    adding new install script

---
+++ install_zsh_plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Used to install plugins for zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search /usr/share/zsh/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions
